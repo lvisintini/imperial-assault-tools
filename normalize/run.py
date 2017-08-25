e191297b8f86aab478c6b41f3c8ddeba0be3ace7
Luis Visintini <lvisintini@gmail.com>
2017-08-25 17:03:16 +0100

    Deployment Group

--- normalize/run.py
+++ normalize/run.py
@@ -12,6 +12,7 @@ class NormalizeImperialData(PipelineHelper):
         base.AddIds(source=SOURCES.UPGRADE),
         tasks.DeploymentCardDataCollector(field_name='faction'),
         tasks.ImageIntegerDataCollector(field_name='deployment_cost', source=SOURCES.DEPLOYMENT),
+        tasks.ImageIntegerDataCollector(field_name='deployment_group', source=SOURCES.DEPLOYMENT),
         tasks.ImageIntegerDataCollector(field_name='reinforce_cost', source=SOURCES.DEPLOYMENT),
         tasks.ImageIntegerDataCollector(field_name='credits', source=SOURCES.UPGRADE),
         base.SaveData('./data/', SOURCES.as_list),
